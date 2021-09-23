2f3473009d215b399e706cdc7b6d4ed09c173d9b
AlexP999 <mikol999@gmail.com>
2021-09-23 13:40:54 +0300

    fixed films.jsx

--- src/components/Films/Films.jsx
+++ src/components/Films/Films.jsx
@@ -43,38 +43,28 @@ export default function Films () {
     };
   };
 
-  // if(postersAllFilms.length === 0) {
-  <nav>
-    { films.map((film) => {
-      return (
-        postersAllFilms.push(
-          <Link to={ `/filmscard/${ film._id }` } >
-            <img key={ film._id } src={ `${ PATHTO.HOST_NAME }/${ film._id }/poster/${ film.poster }` } alt='' />
-          </Link>
-        )
-
-      );
-    }) }
-  </nav>;
-  // }
-
-
   console.log('films render', films);
 
   return (
     <div className="all-films-header">
       <h1>Фильмы</h1>
       <div className="all-films-cont">
+        <nav>
 
-        <div className="all-films-table" >
+          <div className="all-films-table" >
 
-          { films.map((film, i) => {
-            return (
-              <div key={ i } className="posterEl" ><img src={ `${ PATHTO.HOST_NAME }/${ film._id }/poster/${ film.poster }` } alt="" /></div>
-            );
-          }) }
+            { films.map((film, i) => {
+              return (
+                <div key={ i } className="posterEl" >
+                  <Link to={ `/filmscard/${ film._id }` }>
+                    <img src={ `${ PATHTO.HOST_NAME }/${ film._id }/poster/${ film.poster }` } alt="" />
+                  </Link>
+                </div>
+              );
+            }) }
 
-        </div>
+          </div>
+        </nav>
 
       </div>
     </div>
