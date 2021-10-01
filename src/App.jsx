66e5b4b679ec4390cc360fe66484ed6d25c18b18
AlexP999 <mikol999@gmail.com>
2021-10-01 20:28:19 +0300

    new slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,52 @@
 import React from 'react';
 import './App.css';
-import { Slider } from './components/Slider/Slider';
+import Slider from './components/Slider/Slider';
+import { CarouselItem } from './components/Slider/Slider';
+
+import slide1 from "./components/img/slide1.jpg";
+import slide2 from "./components/img/slide2.jpg";
+import slide3 from "./components/img/slide3.jpg";
+import slide4 from "./components/img/slide4.jpg";
+import slide5 from "./components/img/slide5.jpg";
+
+
+const img = [
+  <img src={ slide1 } alt='' />,
+  <img src={ slide2 } alt='' />,
+  <img src={ slide3 } alt='' />,
+  <img src={ slide4 } alt='' />,
+  <img src={ slide5 } alt='' />,
+  <img src={ slide1 } alt='' />,
+  <img src={ slide2 } alt='' />,
+  <img src={ slide3 } alt='' />,
+  <img src={ slide4 } alt='' />,
+  <img src={ slide5 } alt='' />,
+];
 
 function App () {
   return (
-    <div className="App">
-      <Slider />
+    <div>
+      <div className="App">
+        {/* <Slider>
+          { img.map((image, i) => {
+
+            return <CarouselItem>{ image }</CarouselItem>;
+
+          }) }
+
+        </Slider> */}
+      </div>
+      <div className="App1">
+        <Slider>
+          { img.map((image, i) => {
+
+            return <CarouselItem>{ image }</CarouselItem>;
+
+          }) }
+
+        </Slider>
+      </div>
+
     </div>
   );
 }
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,61 +1,123 @@
 
 import React, { useState, useEffect } from 'react';
 import './Slider.css';
-import slide1 from "../img/slide1.jpg";
-import slide2 from "../img/slide2.jpg";
-import slide3 from "../img/slide3.jpg";
-import slide4 from "../img/slide4.jpg";
-import slide5 from "../img/slide5.jpg";
 
 
-const img = [
-  <img key={ slide1 } src={ slide1 } alt='slide1' />,
-  <img key={ slide2 } src={ slide2 } alt='slide2' />,
-  <img key={ slide3 } src={ slide3 } alt='slide3' />,
-  <img key={ slide4 } src={ slide4 } alt='slide4' />,
-  <img key={ slide5 } src={ slide5 } alt='slide5' />,
-];
+export const CarouselItem = ({ children, width }) => {
+  return (
+    <div className='carousel-item' style={ { width: width } }>
+      { children }
+    </div>
+  );
+};
 
-export function Slider () {
+const Slider = ({ children }) => {
   const [ activeIndex, setActiveIndex ] = useState(0);
+  const [ doAutoSlidePaused, setDoAutoSlidePaused ] = useState(false);
 
-  // Хук Effect
+  const updateIndex = (newIndex) => {
+    if(newIndex < 0) {
+      newIndex = React.Children.count(children) - 1;
+
+    } else if(newIndex >= React.Children.count(children)) {
+      newIndex = 0;
+    }
+    setActiveIndex(newIndex);
+  };
+
   useEffect(() => {
-    // Запускаем интервал
-    setInterval(() => {
-      // Меняем состояние
-      setActiveIndex((current) => {
-        // Вычисляем индекс следующего слайда, который должен вывестись
-        const res = current === img.length - 1 ? 0 : current + 1;
-        // Возвращаем индекс
-        return res;
-      });
-    }, 6000);
-    // Выключаем интервал
-    return () => clearInterval();
-  }, []);
+    const interval = setInterval(() => {
+      if(!doAutoSlidePaused) {
+        updateIndex(activeIndex + 1);
+      }
+    }, 3000);
+    return () => {
+      if(interval) {
+        clearInterval(interval);
+      }
+    };
+  });
 
-  // Вычисляем индекс предыдущего слайда
-  const prevImgIndex = activeIndex ? activeIndex - 1 : img.length - 1;
-  // Вычисляем индекс следующего слайда
-  const nextImgIndex = activeIndex === img.length - 1 ? 0 : activeIndex + 1;
 
   return (
-    <div className="slider">
-      <div className="slider-img slider-img-prev"
-        key={ prevImgIndex }
+    <div>
+      {/* <div className='carousel'
+        onMouseEnter={ () => setDoAutoSlidePaused(true) }
+        onMouseLeave={ () => setDoAutoSlidePaused(false) }
       >
-        { img[ prevImgIndex ] }
-      </div>
-      <div className="slider-img"
-        key={ activeIndex }
+        <div className="inner" style={ { transform: `translateX(-${ activeIndex * 50 }%)` } }>
+          { React.Children.map(children, (child, i) => {
+            return React.cloneElement(child, { width: "50%" });
+          }
+          ) }
+        </div>
+        <div className="buttons-slider">
+          <button onClick={ () => updateIndex(activeIndex - 1) }>Prev</button>
+          <button onClick={ () => updateIndex(activeIndex + 1) }>Next</button>
+
+        </div>
+      </div> */}
+      <div className='carousel1'
+        onMouseEnter={ () => setDoAutoSlidePaused(true) }
+        onMouseLeave={ () => setDoAutoSlidePaused(false) }
       >
-        { img[ activeIndex ] }
-      </div>
-      <div className="slider-img slider-img-next"
-        key={ nextImgIndex }>
-        { img[ nextImgIndex ] }
+        <div className="inner1" style={ { transform: `translateX(-${ activeIndex * 15 }%)` } }>
+          { React.Children.map(children, (child, i) => {
+            return React.cloneElement(child, { width: "15%" });
+          }
+          ) }
+        </div>
+        <div className="buttons-slider1">
+          <button onClick={ () => updateIndex(activeIndex - 1) }>Prev</button>
+          <button onClick={ () => updateIndex(activeIndex + 1) }>Next</button>
+
+        </div>
       </div>
     </div>
   );
-}
+};
+export default Slider;
+
+
+//   const [ activeIndex, setActiveIndex ] = useState(0);
+
+//   // Хук Effect
+//   useEffect(() => {
+//     // Запускаем интервал
+//     setInterval(() => {
+//       // Меняем состояние
+//       setActiveIndex((current) => {
+//         // Вычисляем индекс следующего слайда, который должен вывестись
+//         const res = current === img.length - 1 ? 0 : current + 1;
+//         // Возвращаем индекс
+//         return res;
+//       });
+//     }, 6000);
+//     // Выключаем интервал
+//     return () => clearInterval();
+//   }, []);
+
+//   // Вычисляем индекс предыдущего слайда
+//   const prevImgIndex = activeIndex ? activeIndex - 1 : img.length - 1;
+//   // Вычисляем индекс следующего слайда
+//   const nextImgIndex = activeIndex === img.length - 1 ? 0 : activeIndex + 1;
+
+//   return (
+//     <div className="slider">
+//       <div className="slider-img slider-img-prev"
+//         key={ prevImgIndex }
+//       >
+//         { img[ prevImgIndex ] }
+//       </div>
+//       <div className="slider-img"
+//         key={ activeIndex }
+//       >
+//         { img[ activeIndex ] }
+//       </div>
+//       <div className="slider-img slider-img-next"
+//         key={ nextImgIndex }>
+//         { img[ nextImgIndex ] }
+//       </div>
+//     </div>
+//   );
+// }
